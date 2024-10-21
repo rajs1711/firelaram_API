2211ca7098b08c383fa363e093881157e16e079c
saurav <64578987+rajs1711@users.noreply.github.com>
2024-10-22 00:41:23 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 //require('dotenv').config();
 
 module.exports.DATABASE = mysql.createPool({
-    "host": 'b8hdltuyfxz4qrzs6cif-mysql.services.clever-cloud.com'//'localhost',
-    "user": 'u5iyfpvqpdz8px8q'//'firealram',//root
-    "password":'0nYJTEGc2Dg2XZluGhMx' //'Indi@12345',
-    "database": 'b8hdltuyfxz4qrzs6cif'//'firealarm',
-    //"timezone": //'Z',
+    "host": 'b8hdltuyfxz4qrzs6cif-mysql.services.clever-cloud.com',//'localhost',
+    "user": 'u5iyfpvqpdz8px8q',//'firealram',//root
+    "password":'0nYJTEGc2Dg2XZluGhMx', //'Indi@12345',
+    "database": 'b8hdltuyfxz4qrzs6cif',//'firealarm',
+    //"timezone": //'Z'
     "port": 3306//process.env.DB_PORT
 });
 
