4ff57d386ae8a0b34c750efba4550dc348ff1715
saurav <64578987+rajs1711@users.noreply.github.com>
2024-10-23 00:07:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,6 @@
 'use strict'
 const fastify = require('fastify')();
-/*const fastify = require('fastify')({
-    bodyLimit: 1048576 * 2,
-   // logger: { prettyPrint: true, level: 'info', file: '/home/pratyush/nodeServer/logs/backend_server/backend.log' },
-});
 
-
-const redis = require('redis').createClient({ host: '127.0.0.1' })
-
-fastify
-  .register(require('fastify-redis'), {
-      host: '127.0.0.1',
-      namespace: 'pubsub'
-  })
-  .register(require('fastify-redis'), {
-      client: redis,
-      namespace: 'normal'
-  });
-*/
 
 fastify.register(require('fastify-cors'), { 
   // put your options here
@@ -25,11 +8,10 @@ fastify.register(require('fastify-cors'), {
   methods: ['GET', 'PUT', 'POST']
 });
 
-//fastify.register(require('./plugins/nodeIPCPlugin'));
 fastify.register(require('fastify-bcrypt'),{ saltWorkFactor: 10 });
 fastify.register(require('fastify-jwt'), { secret: 'Indi12345Pt29Satqsknm5629nmvxdskl074vb' , sign: { expiresIn:"1d" }});
 fastify.register(require('fastify-auth'));
-fastify.register(require('./routes/api/index'), { prefix: '/api' });
+fastify.register(require('./routes/api'), { prefix: 'api' });
 fastify.get('/',async(req,res)=>{
     return 'success';
 });
@@ -37,9 +19,8 @@ fastify.get('/',async(req,res)=>{
 module.exports = async (req, res) => {
   await fastify.ready().then(() => {
   console.log(fastify.printRoutes());
-}); // Ensure Fastify is ready to handle requests
+}); 
+// Ensure Fastify is ready to handle requests
   fastify.server.emit('request', req, res); // Pass the request to Fastify's HTTP server
 };
 
-
-//module.exports = fastify
