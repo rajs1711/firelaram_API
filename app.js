2d07e06c2d4848068dcf6a25ef7d033c42402fa9
saurav <64578987+rajs1711@users.noreply.github.com>
2024-10-23 00:33:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const fastify = require('fastify')();
-fastify.register(require('./routes/api'), { prefix: '/myapi' });
+fastify.register(require('./routes/api'));
 //fastify.register(require('fastify-bcrypt'),{ saltWorkFactor: 10 });
 //fastify.register(require('fastify-jwt'), { secret: 'Indi12345Pt29Satqsknm5629nmvxdskl074vb' , sign: { expiresIn:"1d" }});
 //fastify.register(require('fastify-auth'));
